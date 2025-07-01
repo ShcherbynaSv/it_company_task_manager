4eb0fb26ef08cb7c4f3d3e239029c46181a36464
Svitlana Shcherbyna <sw.shcherbina@gmail.com>
2025-07-01 17:34:25 +0300

    Customize admim panel

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from tasks.models import Position, Team, TaskType, Tag, Project, Worker, Task
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ["name", "created_at", "description", "team"]
+    list_filter = ["name", "created_at", "team"]
+    search_fields = ["name"]
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position", "team")
+    list_filter = UserAdmin.list_filter + ("position", "team")
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("position", "team")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Additional info",
+            {"fields": ("first_name", "last_name", "position", "team")}
+        ),
+    )
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "created_at",
+        "deadline",
+        "project",
+        "is_completed",
+        "priority",
+        "task_type"
+    ]
+
+
+admin.site.register(Position)
+admin.site.register(Team)
+admin.site.register(TaskType)
+admin.site.register(Tag)
