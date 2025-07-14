08193dfa582e7af9092176af702a23f351b345cb
Svitlana Shcherbyna <sw.shcherbina@gmail.com>
2025-07-14 15:04:33 +0300

    Add templates and includes folders with base.html, index.html, sidebar.html files. Change index view to render index.html template

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,17 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from .models import Tag, Task, TaskType, Team, Project, Position, Worker
+
 
 def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("<h1>Task Manager</h1>")
+    context = {
+        "tags": Tag.objects.all().count(),
+        "tasks": Task.objects.all().count(),
+        "task_types": TaskType.objects.all().count(),
+        "teams": Team.objects.all().count(),
+        "projects": Project.objects.all().count(),
+        "positions": Position.objects.all().count(),
+        "workers": Worker.objects.all().count()
+    }
+    return render(request, "tasks/index.html", context=context)
