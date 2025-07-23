1a9edfe9d48eef20f5d79c907f796275cb73ea12
Svitlana Shcherbyna <sw.shcherbina@gmail.com>
2025-07-23 19:05:43 +0300

    Create urls, views and templates for tags, tasks, task types, teams, projects, positions and workers lists pages

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,25 @@
 from django.urls import path
 
-from tasks.views import index
+from tasks.views import (
+    index,
+    PositionListView,
+    TeamListView,
+    TaskTypeListView,
+    TagListView,
+    ProjectListView,
+    WorkerListView,
+    TaskListView,
+)
 
 urlpatterns = [
     path("", index, name="index"),
+    path("positions/", PositionListView.as_view(), name="position-list"),
+    path("teams/", TeamListView.as_view(), name="team-list"),
+    path("task-types/", TaskTypeListView.as_view(), name="task-type-list"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("projects/", ProjectListView.as_view(), name="project-list"),
+    path("workers/", WorkerListView.as_view(), name="worker-list"),
+    path("tasks", TaskListView.as_view(), name="task-list"),
 ]
 
 app_name = "tasks"
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views import generic
 
 from .models import Tag, Task, TaskType, Team, Project, Position, Worker
 
@@ -15,3 +16,50 @@ def index(request: HttpRequest) -> HttpResponse:
         "workers": Worker.objects.all().count()
     }
     return render(request, "tasks/index.html", context=context)
+
+
+class PositionListView(generic.ListView):
+    model = Position
+
+
+class TeamListView(generic.ListView):
+    model = Team
+
+
+class TaskTypeListView(generic.ListView):
+    model = TaskType
+    template_name = "tasks/task_type_list.html"
+    context_object_name = "task_type_list"
+
+
+class TagListView(generic.ListView):
+    model = Tag
+
+
+class ProjectListView(generic.ListView):
+    model = Project
+
+
+class WorkerListView(generic.ListView):
+    model = Worker
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        position_id = self.request.GET.get("position")
+        if position_id:
+            queryset = queryset.filter(position_id=position_id)
+        return queryset
+
+
+class TaskListView(generic.ListView):
+    model = Task
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        tag_id = self.request.GET.get("tags")
+        task_type_id = self.request.GET.get("task_type")
+        if tag_id:
+            queryset = queryset.filter(tags__id=tag_id)
+        if task_type_id:
+            queryset = queryset.filter(task_type_id=task_type_id)
+        return queryset
