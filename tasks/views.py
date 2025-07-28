3746d935ec06f031b8748314222b4e3b0b4529c8
Svitlana Shcherbyna <sw.shcherbina@gmail.com>
2025-07-28 09:26:06 +0300

    Add possibility to clear the filter on task and worker list pages if they are filtered by tag, task type or profession

--- tasks/views.py
+++ tasks/views.py
@@ -45,10 +45,20 @@ class WorkerListView(generic.ListView):
 
     def get_queryset(self):
         queryset = super().get_queryset()
+        self.position = None
         position_id = self.request.GET.get("position")
         if position_id:
             queryset = queryset.filter(position_id=position_id)
+            try:
+                self.position = Position.objects.get(id=position_id)
+            except Position.DoesNotExist:
+                self.position = None
         return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["selected_position"] = self.position
+        return context
 
 
 class TaskListView(generic.ListView):
@@ -56,10 +66,27 @@ class TaskListView(generic.ListView):
 
     def get_queryset(self):
         queryset = super().get_queryset()
+        self.tag = None
+        self.task_type = None
+
         tag_id = self.request.GET.get("tags")
         task_type_id = self.request.GET.get("task_type")
         if tag_id:
             queryset = queryset.filter(tags__id=tag_id)
+            try:
+                self.tag = Tag.objects.get(id=tag_id)
+            except Tag.DoesNotExist:
+                self.tag = None
         if task_type_id:
             queryset = queryset.filter(task_type_id=task_type_id)
+            try:
+                self.task_type = TaskType.objects.get(id=task_type_id)
+            except TaskType.DoesNotExist:
+                self.task_type = None
         return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["selected_tag"] = self.tag
+        context["selected_task_type"] = self.task_type
+        return context
