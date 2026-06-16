6b6ddf73f335f82f5bf1c6e1bf020964b8c08c8a
Svitlana Shcherbyna <sw.shcherbina@gmail.com>
2026-06-16 22:28:17 +0300

    Prepare for renering

--- it_company_task_manager/settings/prod.py
+++ it_company_task_manager/settings/prod.py
@@ -7,6 +7,9 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME:
+   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
@@ -19,8 +22,8 @@ DATABASES = {
         "PASSWORD": os.environ["POSTGRES_PASSWORD"],
         "HOST": os.environ["POSTGRES_HOST"],
         "PORT": int(os.environ["POSTGRES_DB_PORT"]),
-        # "OPTIONS": {
-        #     "sslmode": "require",
-        # },
+        "OPTIONS": {
+            "sslmode": "require",
+        },
     }
 }
